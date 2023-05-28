556f88aae9b5cf0963ef9167902403608a5e0ef2
Noah Hirst-Ashuach <nhirstashuach@gmail.com>
2023-05-28 21:04:37 +0100

    Added method to get piece from letter

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,5 +30,37 @@ public struct Piece
     {
         Type = type;
         Colour = colour;
+    }
+
+    public static Piece GetPieceFromLetter(char letter)
+    {
+        Piece piece = new Piece();
+
+        // Dictionary connecting piece names to their type
+        var pieceDict = new Dictionary<char, PieceType>
+        {
+            { 'p', PieceType.Pawn },
+            { 'r', PieceType.Rook },
+            { 'n', PieceType.Knight },
+            { 'b', PieceType.Bishop },
+            { 'q', PieceType.Queen },
+            { 'k', PieceType.King },
+        };
+
+        if (!pieceDict.ContainsKey(Char.ToLower(letter)))
+            throw new ArgumentException($"Letter not recognized: {letter}", nameof(letter));
+
+        piece.Type = pieceDict[Char.ToLower(letter)];
+
+        if (Char.IsUpper(letter))
+        {
+            piece.Colour = Colour.Black;
+        }
+        else
+        {
+            piece.Colour = Colour.White;
+        }
+
+        return piece;
     }
 }
