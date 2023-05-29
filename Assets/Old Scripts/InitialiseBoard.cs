eab8b70dd84567897704b57d60187be3611b5236
Noah Hirst-Ashuach <nhirstashuach@gmail.com>
2023-05-29 18:32:42 +0100

    Deleted old scripts

--- Assets/Old Scripts/InitialiseBoard.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Initialises the board at the start of a game.
-/// </summary>
-public class InitialiseBoard : MonoBehaviour
-{
-    private void Start() => Board.GeneratePosition(Board.startingPosition);
-}
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,4 +4,5 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    [SerializeField] public Dictionary<char, GameObject> pieces;
 }
