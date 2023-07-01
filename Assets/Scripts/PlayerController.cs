f6ec8dfd28e5db82214d37da91453315a0c343a2
Noah Hirst-Ashuach <nhirstashuach@gmail.com>
2023-07-01 16:02:01 +0100

    Added PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// Handles player inputs.
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    // Wait for a click to happen, then on click, record the current state of the board and the legal moves for
+    // the piece that was clicked, then on click up, check if the board state is in the list of legal board
+    // states, if not then reset the board to how it was.
+
+    // Also on click highlight all legal move squares for the clicked piece
+}
