63ba04a6de56499cdf24ba6dc0f63683c97fc65a
Noah Hirst-Ashuach <nhirstashuach@gmail.com>
2023-07-01 16:02:34 +0100

    Undid changes to PieceID

--- Assets/Scripts/PieceID.cs
+++ Assets/Scripts/PieceID.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 /// <summary>
 /// A script to attach to piece prefabs to identify what type of piece they are.
 /// </summary>
-public class PieceID
+public class PieceID : MonoBehaviour
 {
     [SerializeField] private PieceType type;
     [SerializeField] private PieceColour colour;
-    public Piece Piece => new Piece(type, colour);
+    public Piece Piece { get; private set; }
+
+    private void Awake()
+    {
+        Piece = new Piece(type, colour);
+    }
 }
