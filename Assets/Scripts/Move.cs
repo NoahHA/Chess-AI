6fc075825d774754f6c2d268abb46b54752c2e5b
Noah Hirst-Ashuach <nhirstashuach@gmail.com>
2023-07-02 15:46:11 +0100

    Gave Move class value equality

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -24,4 +24,30 @@ public class Move
     {
         return StartSquare.ToString() + EndSquare.ToString();
     }
+
+    public override bool Equals(object obj)
+    {
+        //Check for null and compare run-time types.
+        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+        {
+            return false;
+        }
+
+        return this.ToString() == obj.ToString();
+    }
+
+    public static bool operator ==(Move obj1, Move obj2)
+    {
+        return obj1.ToString() == obj2.ToString();
+    }
+
+    public static bool operator !=(Move obj1, Move obj2)
+    {
+        return obj1.ToString() != obj2.ToString();
+    }
+
+    public override int GetHashCode()
+    {
+        return StartSquare.GetHashCode() ^ EndSquare.GetHashCode();
+    }
 }
